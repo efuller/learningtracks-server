59a94c97fc0d39406b074394b21fdd2cbaf16388
Eric Fuller <info@glassmoonmedia.com>
2018-02-14 06:40:48 -0500

    Setup middleware to check if logged in. Setup a test protected route.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 import authRoutes from './authRoutes';
 import userRoutes from './userRoutes';
+import isLoggedIn from '../middleware/isLoggedIn';
 
 export default function(app) {
     app.use('/api/auth', authRoutes);
     app.use('/api/users', userRoutes);
+    app.get('/protected', isLoggedIn, (req, res) => {
+        res.json({ success: 'You are authorized!' });
+    })
 }
