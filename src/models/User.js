b9664120fe6f0e7b466891a86a78c7e627c21207
Eric Fuller <info@glassmoonmedia.com>
2018-02-13 21:15:06 -0500

    Add in some user methods we will need to all things authentication.

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
 
 const Schema = mongoose.Schema;
 
@@ -22,5 +25,33 @@ const UserSchema = new Schema({
     }},
     { timestamps: true }
 );
+
+UserSchema.methods.isValidPassword = function isValidPassword(password) {
+    return bcrypt.compareSync(password, this.passwordHash);
+};
+
+UserSchema.methods.setPassword = function setPassword(password) {
+    this.passwordHash = bcrypt.hashSync(password, 10);
+};
+
+UserSchema.methods.generateJWT = function generateJWT() {
+    return jwt.sign({
+        email: this.email,
+        firstName: this.firstName
+    });
+};
+
+UserSchema.methods.toAuthJSON = function toAuthJSON() {
+    return {
+        email: this.email,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        token: this.generateJWT()
+    }
+};
+
+UserSchema.plugin(uniqueValidator, {
+    message: 'Already taken. Please try another!'
+});
 
 export default mongoose.model('User', UserSchema )
